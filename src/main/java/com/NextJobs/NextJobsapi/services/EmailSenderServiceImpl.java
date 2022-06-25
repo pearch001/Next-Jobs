b4a53baa2a51423d7d63714d216035fec1ead4aa
DESKTOP-OJ1GR5E\HP <kasimoluwasegun@gmail.com>
2022-06-26 00:10:29 +0100

    Added Email Sender classes

--- src/main/java/com/NextJobs/NextJobsapi/services/EmailSenderServiceImpl.java
+++ src/main/java/com/NextJobs/NextJobsapi/services/EmailSenderServiceImpl.java
@@ -1,4 +1,38 @@
 package com.NextJobs.NextJobsapi.services;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+@Slf4j
 public class EmailSenderServiceImpl implements EmailSenderServiceInt{
+    private final JavaMailSender mailSender;
+
+
+
+    @Async
+    @Override
+    public void send(String to, String email) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper =
+                    new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("kasimoluwasegun@gmail.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            log.error("failed to send email", e);
+            throw new IllegalStateException("failed to send email");
+        }
+    }
 }
--- src/main/java/com/NextJobs/NextJobsapi/services/EmailSenderServiceInt.java
+++ src/main/java/com/NextJobs/NextJobsapi/services/EmailSenderServiceInt.java
@@ -1,4 +1,5 @@
 package com.NextJobs.NextJobsapi.services;
 
 public interface EmailSenderServiceInt {
+    void send(String to, String email);
 }
